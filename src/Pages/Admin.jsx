b7ca59b01a1ce5f8c2687db8bff071e3a146b2a6
Ben-Weber <benfweber@gmail.com>
2021-08-05 12:16:40 +0300

    Admin Page

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -4,23 +4,32 @@ import "./pages.css";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { useHistory } from "react-router-dom";
 
 const schema = yup.object().shape({
-  Type: yup.string(),
-  Name: yup.string(),
-  Breed: yup.string(),
-  Bio: yup.string(),
-  Dietary: yup.string(),
-  Height: yup.number(),
-  Weight: yup.number(),
-  Color: yup.string(),
+  Type: yup.string().required(),
+  Name: yup.string().required(),
+  Breed: yup.string().required(),
+  Bio: yup.string().required(),
+  Dietary: yup.string().required(),
+  Height: yup.number().required("No password provided.").positive(),
+  Weight: yup.number().positive().integer().label("Phone Number"),
+  Color: yup.string().required(),
+  Adoption: yup.string(),
+  Hypoallergenic: yup.boolean(),
 });
 
 function Admin() {
-  const { register, handleSubmit } = useForm({ resolver: yupResolver(schema) });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+  const history = useHistory();
 
   const onSubmit = (data) => {
     console.log(data);
+    history.push("/petPage");
   };
 
   return (
@@ -29,53 +38,72 @@ function Admin() {
       <div className="form-container">
         <Form>
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Type</Form.Label>
-              <Form.Control type="text" />
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>Type</Form.Label>{" "}
+              <Form.Select
+                // id="Type"
+                {...register("Type")}
+                defaultValue="Choose..."
+              >
+                <option>Dog</option>
+                <option>Cat</option>
+                <option>Other</option>
+              </Form.Select>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridPassword">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control {...register("Name")} type="text" />
+              <span style={{ color: "red" }}>{errors.Name?.message}</span>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridPassword">
               <Form.Label>Breed of animal</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control {...register("Breed")} type="text" />
+              <span style={{ color: "red" }}>{errors.Breed?.message}</span>
             </Form.Group>
           </Row>
 
           <Row className="mb-3">
             <Form.Group as={Col} className="mb-3" controlId="formGridAddress1">
               <Form.Label>Bio</Form.Label>
-              <Form.Control />
+              <Form.Control {...register("Bio")} type="text" />
+              <span style={{ color: "red" }}>{errors.Bio?.message}</span>
             </Form.Group>
 
             <Form.Group as={Col} className="mb-3" controlId="formGridAddress2">
               <Form.Label>Dietary restrictions</Form.Label>
-              <Form.Control />
+              <Form.Control {...register("Dietary")} type="text" />
+              <span style={{ color: "red" }}>{errors.Dietary?.message}</span>
             </Form.Group>
           </Row>
 
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>Height</Form.Label>
-              <Form.Control />
+              <Form.Control {...register("Height")} type="number" />
+              <span style={{ color: "red" }}>
+                {errors.Weight && "Height is required field"}
+              </span>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridZip">
               <Form.Label>Weight</Form.Label>
-              <Form.Control />
+              <Form.Control {...register("Weight")} type="number" />
+              <span style={{ color: "red" }}>
+                {errors.Weight && "Wight is required field"}
+              </span>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridZip">
               <Form.Label>Color</Form.Label>
-              <Form.Control />
+              <Form.Control {...register("Color")} type="text" />
+              <span style={{ color: "red" }}>{errors.Color?.message}</span>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridState">
               <Form.Label>Adoption Status</Form.Label>
-              <Form.Select defaultValue="Choose...">
+              <Form.Select {...register("Adoption")} defaultValue="Choose...">
                 <option>Adopted</option>
                 <option>Fostered</option>
               </Form.Select>
@@ -88,11 +116,20 @@ function Admin() {
           </Form.Group>
 
           <Form.Group className="mb-3" id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Hypoallergenic?" />
+            <Form.Check
+              {...register("Hypoallergenic")}
+              type="checkbox"
+              label="Hypoallergenic?"
+              isValid
+            />
           </Form.Group>
 
-          <Button variant="success" type="submit">
-            Submit
+          <Button
+            onClick={handleSubmit(onSubmit)}
+            variant="success"
+            type="submit"
+          >
+            Add Pet
           </Button>
         </Form>
       </div>
