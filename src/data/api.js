29aecb3d45c63c2d006c397ae6f0b80d7c0f6d1d
Ben-Weber <benfweber@gmail.com>
2021-08-10 13:46:21 +0300

    changed route to /signup (from /addNewUser)

--- src/data/api.js
+++ src/data/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const registerUser = async (userData) => {
   try {
     const response = await axios.post(
-      "http://localhost:4000/user/addNewUser",
+      "http://localhost:4000/user/signup",
       userData
     );
     console.log("FE axios.post 'response' from api.js:", response);
